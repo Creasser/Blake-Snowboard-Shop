2c233b94b00e7460fd96c7d4805e21f90892294f
Blake Creasser <blakecreasser@aggienetwork.com>
2022-11-29 12:59:37 -0600

    add el for dom content, made fetch funtion

---
+++ scripts.js
@@ -0,0 +1,11 @@
+//use DOMContentLoaded to fetch the toys AFTER the DOM has loaded, this is to prevent any errors happening before the DOM has loaded
+document.addEventListener('DOMContentLoaded', fetchSnowboards)
+
+//grab the toys using fetch GET
+
+function fetchSnowboards(){
+    fetch('http://localhost:3000/snowboards')
+    .then(resp => resp.json())
+    .then(boardData => console.log(boardData))
+}
+//fetchSnowboards()
